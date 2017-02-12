35d116b0d182afb42ad800fe20eaab36d04cceda
Pearson <w.policiano@gmail.com>
2017-02-12 03:29:49 -0200

    Tela do filme adicionada

---
+++ SearTV/MovieDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsViewController.swift
+//  SearTV
+//
+//  Created by Pearson on 12/02/17.
+//  Copyright © 2017 Pearson. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  
+}
